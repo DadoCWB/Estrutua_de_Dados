4d599a36e967eb78f387fb8888f2669a45549fc2
Dado CWB <44261704+DadoCWB@users.noreply.github.com>
2019-03-30 08:42:08 -0300

    Add files via upload

---
+++ teste.c
@@ -0,0 +1,44 @@
+/*Receber dados a partir de um arquivo
+  Se você encontrar alguém com dificuldades repasse esse código para ele(a)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	
+	int i;			// contador 
+	char txt[50];	//string para armazera os caracteres de cada linha lida dento do laço pelo fscanf  
+					//estou supondo que uma linha não irá conter mais que 50 caracteres
+	
+	FILE *arq; //Ponteiro para abrir o arquivo txt
+	
+	arq=fopen("NomeRG10.txt", "r"); //nome do arquivo a ser aberto
+									// o "r" significa que o arquivo será aberto em modo leitura 
+	
+	for(i=0;i<3;i++){
+		fscanf(arq,"%s",txt); // lê uma linha do arquivo e armazena-a na string txt
+		
+		printf("\n%s",txt); //imprime na tela cada uma das linhas que foi lida pelo fscanf
+	}
+		
+};
+
+/*
+Explicando um pouco as coisas:
+	
+	O ponteiro na linha 13 é do tipo FILE. Ele é usado para varrer cada caractere contido no arquivo e
+	armazená-los na string txt.
+ 
+	fscanf é bem parecido com scanf . A diferença é que scanf lê o teclado e fscanf lê um arquivo.
+	
+	para ler TODAS as linhas do arquivo ao invés de 3 recomenda-se:
+	
+	while(!feof(arq)){ // le o arquivo até a ultima linha
+  		fscanf(arq,"%s",txt); // lê uma linha do arquivo e armazena-a na string txt
+		
+		printf("\n%s",txt); //imprime na tela cada uma das linhas que foi lida pelo fscanf 
+	}
+	
+*/
